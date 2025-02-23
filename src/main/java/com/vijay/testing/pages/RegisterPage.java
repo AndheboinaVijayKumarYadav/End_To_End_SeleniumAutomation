29104f6ed88264bc500cd677cb607bcc6afe640a
AndheboinaVijayKumarYadav <andheboinavijaykumaryadav@gmail.com>
2025-02-23 08:17:50 +0530

    Created a negative scenarios where different data is given using Data provider in TestNG

--- src/main/java/com/vijay/testing/pages/RegisterPage.java
+++ src/main/java/com/vijay/testing/pages/RegisterPage.java
@@ -2,6 +2,12 @@ package com.vijay.testing.pages;
 
 import com.vijay.testing.base.CommonToAllPage;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.vijay.testing.driver.DriverManager.driver;
 
 public class RegisterPage extends LandingPage {
 
@@ -16,6 +22,9 @@ public class RegisterPage extends LandingPage {
     private By continueButton = By.xpath("//input[@value='Continue']");
 
 
+
+    //Page Actions
+    // below method returns the CreatedPage all the form details given are valid ones
     public CreatedPage registerAccount(String firstName,String lastName,String email,String telephone,String password,String confirmPassword){
         enterText(inputFirstNameField, firstName);
         enterText(inputLastNameField, lastName);
@@ -34,7 +43,26 @@ public class RegisterPage extends LandingPage {
         return new CreatedPage();
     }
 
+    // general method for filling form details
+    public void fillForm(String firstName,String lastName,String email,String telephone,String password, String confirmPassword){
 
+        enterText(inputFirstNameField, firstName);
+        enterText(inputLastNameField, lastName);
+        enterText(inputEmailField, email);
+        enterText(inputTelephoneField, telephone);
+        enterText(inputPasswordField, password);
+        enterText(inputConfirmPasswordField, confirmPassword);
+    }
+
+    // collecting all errormessages
+    public List<String> getAllErrorMessages(){
+        List<WebElement> errorElements = driver.findElements(By.cssSelector(".text-danger"));
+        return errorElements.stream().map(WebElement::getText).collect(Collectors.toList());
+    }
+
+    public void clickOnContinue(){
+        clickElement(continueButton);
+    }
 
 
 }
--- src/test/java/com/vijay/testing/tests/tutorialsNinja/RegisterTest.java
+++ src/test/java/com/vijay/testing/tests/tutorialsNinja/RegisterTest.java
@@ -6,17 +6,35 @@ import com.vijay.testing.pages.CreatedPage;
 import com.vijay.testing.pages.LandingPage;
 import com.vijay.testing.pages.RegisterPage;
 import com.vijay.testing.utils.PropertiesReader;
+import io.qameta.allure.Description;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
 
 public class RegisterTest extends CommonToAllTest {
     private static final Logger logger = LogManager.getLogger(RegisterTest.class);
 
+    @DataProvider(name = "formCombinations")
+    public Object[][] getFromCombinations(){
+        return new Object[][]{
+                {"", "", "", "", "", new String[]{"First Name must be between 1 and 32 characters!", "Last Name must be between 1 and 32 characters!", "E-Mail Address does not appear to be valid!", "Telephone must be between 3 and 32 characters!", "Password must be between 4 and 20 characters!"}},
+                {"John", "", "", "", "", new String[]{"Last Name must be between 1 and 32 characters!", "E-Mail Address does not appear to be valid!", "Telephone must be between 3 and 32 characters!", "Password must be between 4 and 20 characters!"}},
+                {"John", "Doe", "", "", "", new String[]{"E-Mail Address does not appear to be valid!", "Telephone must be between 3 and 32 characters!", "Password must be between 4 and 20 characters!"}},
+                {"John", "Doe", "john.doe@gmail.com", "", "", new String[]{"Telephone must be between 3 and 32 characters!", "Password must be between 4 and 20 characters!"}},
+                {"John", "Doe", "john.doe@gmail.com", "1234567890", "", new String[]{"Password must be between 4 and 20 characters!"}},
+                {"John", "Doe", "john.doe@gmail.com", "1234567890", "Password123", new String[]{}},  // No errors expected
+        };
+
+    }
+
+
+    @Description("Test Case 1: Verification of registering the account with valid details")
     @Test
     public void testRegisterAccountWithValidCredentials(){
 
@@ -31,10 +49,12 @@ public class RegisterTest extends CommonToAllTest {
        logger.info("Navigated to Register Page");
 
 
+       // email should be unique so created below string and passed as parameter
+       String email = "vijay" + System.currentTimeMillis() + "@gmail.com";
 
        CreatedPage createdPage = registerPage.registerAccount(PropertiesReader.readKey("firstname"),
                PropertiesReader.readKey("lastname"),
-               PropertiesReader.readKey("email"),
+               email,
                PropertiesReader.readKey("telephone"),
                PropertiesReader.readKey("password"),
                PropertiesReader.readKey("confirm_password") );
@@ -42,7 +62,41 @@ public class RegisterTest extends CommonToAllTest {
 
         Assert.assertTrue(createdPage.isElementVisible(),"\n Value do not match\n");
         logger.info("Registration successful and confirmation page displayed");
+
     }
+
+    @Description("Test Case 2: Verification of fill form with invalid combinations")
+    @Test(dataProvider = "formCombinations")
+    public void testFormFillingInvalidCombinations(String firstName, String lastName, String email, String phone, String password, String[] expectedErrors){
+        logger.info("Starting test: Register Account With InValid Credentials");
+
+        DriverManager.getDriver().get(PropertiesReader.readKey("url"));
+        logger.info("Navigated to URL");
+
+        LandingPage landingPage = new LandingPage();
+        landingPage.goToAccountMenu();
+        RegisterPage registerPage = landingPage.clickRegisterMenu();
+        logger.info("Navigated to Register Page");
+
+        registerPage.clickOnContinue();
+
+        // SoftAssert to capture all errors
+        SoftAssert softAssert = new SoftAssert();
+
+        // Verify each expected error
+        for (String expectedError : expectedErrors) {
+            boolean errorFound = registerPage.getAllErrorMessages().contains(expectedError);
+            softAssert.assertTrue(errorFound, "Expected error not found: " + expectedError);
+        }
+
+
+        // Assert all
+        softAssert.assertAll();
+
+
+
+    }
+
 
 
 }
