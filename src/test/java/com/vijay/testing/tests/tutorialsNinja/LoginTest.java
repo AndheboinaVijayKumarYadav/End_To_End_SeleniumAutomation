e558bc70b1fd4e6f3a2ec529c333c1f145b4af9f
AndheboinaVijayKumarYadav <andheboinavijaykumaryadav@gmail.com>
2025-05-06 16:40:00 +0530

    Add this search test to Login test to run through Jenkins

--- src/test/java/com/vijay/testing/tests/tutorialsNinja/LoginTest.java
+++ src/test/java/com/vijay/testing/tests/tutorialsNinja/LoginTest.java
@@ -1,6 +1,7 @@
 package com.vijay.testing.tests.tutorialsNinja;
 
 import com.vijay.testing.base.CommonToAllTest;
+import com.vijay.testing.driver.DriverManager;
 import com.vijay.testing.pages.*;
 import com.vijay.testing.utils.PropertiesReader;
 import io.qameta.allure.Description;
@@ -62,6 +63,24 @@ public class LoginTest extends CommonToAllTest {
         Assert.assertEquals(actualText,expectedText,"Actual warning message is not matched with expected warning message");
 
 
+
+    }
+    @Description("TestCase 3: Verify the Search with existing product")
+    @Test
+    public void testSearchWithExistingProduct(){
+
+        String productTitle = PropertiesReader.readKey("title");
+        DriverManager.getDriver().get(PropertiesReader.readKey("url"));
+        LandingPage landingPage = new LandingPage();
+        SearchPage searchPage = landingPage.searching(productTitle);
+
+        String expectedProductTitle = PropertiesReader.readKey("title");
+        String actualProductTitle = searchPage.getTitleOfProduct();
+        String expectedDescription = "Just when you thought iMac had everything, now there´s even more. More powerful Intel Core 2 Duo pro..";
+        String actualDescription = searchPage.getDescriptionOfProduct();
+
+        Assert.assertEquals(actualProductTitle,expectedProductTitle,"Product tile do not match");
+        Assert.assertEquals(actualDescription,expectedDescription,"Product descriptions do not match");
 
     }
 
